8ded8a1c57562cb1bf3f72d3eff31f98dfda1b83
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-09-27 06:47:59 +0200

    randomizeArray

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -11,7 +11,7 @@
     >
       <label
         :for="questions[progress].choices[index]"
-        v-for="(item, index) in questions[progress].choices"
+        v-for="(item, index) in mixedArray"
         :key="index"
       >
         <input
@@ -27,12 +27,37 @@
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted } from "vue";
 import type { Question } from "./quiz.vue";
 
-defineProps<{
+const props = defineProps<{
   questions: Question[];
   progress: number;
 }>();
+
+const randomizeArray = (array: string[]): string[] => {
+  // créer un tableau intermédiaire de 0 à nombreOfElements. ex: [0,1,2,3]
+  let newArray: number[] = [];
+  let returnArray: string[] = [];
+  let arrayLength = array.length;
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(i);
+  }
+  while (arrayLength > 0) {
+    // prendre un element au hasard dans le tableau. ex : 2
+    let randomIndex = Math.floor(Math.random() * arrayLength);
+    // prendre l'element du tableau de départ a l'index 2 et le push dans le tableau final
+    let randomElement = array.splice(randomIndex, 1);
+    returnArray.push(randomElement[0]);
+    arrayLength--;
+  }
+  return returnArray;
+};
+
+const mixedArray = computed(() => {
+  return randomizeArray(props.questions[props.progress].choices);
+});
 
 const emit = defineEmits(["answerSelected"]);
 const answerSelected = (answer: string) => emit("answerSelected", answer);
